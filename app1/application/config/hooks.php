cf35d91f77a7c4087abd0df9c56f6020ca0e8464
antho-firuze <antho.firuze@gmail.com>
2017-10-02 07:24:08 +0700

    Updates

--- app1/application/config/hooks.php
+++ app1/application/config/hooks.php
@@ -11,15 +11,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
 $hook['pre_controller'] = function()
 {
-	// $file = APPPATH.'logs/test.txt';
-	// if (! file_exists($file))
-		// file_put_contents($file, '');
+};
+
+$hook['post_controller_constructor'] = function()
+{
+	if(isset($_SESSION['screen_width']) AND isset($_SESSION['screen_height'])){
+		// $data['screen_res'] = $_SESSION['screen_width'] .'x'. $_SESSION['screen_height'];
+		
+		// $file = APPPATH.'logs/access_log.txt';
+		// if (! file_exists($file))
+			// file_put_contents($file, '');
+		
+		// $str = file_get_contents($file);
+		
+		// $ci =& get_instance();
+		// $ci->load->helper('z_libs/common');
+		// $ci->load->library('user_agent');
+		
+		// $data['ip_address'] = get_ip_address();
+		// $data['method'] = $_SERVER['REQUEST_METHOD'];
+		// $data['request_uri'] = $_SERVER['REQUEST_URI'];
+		// $data['platform'] = $ci->agent->platform();
+		// $data['is_mobile'] = $ci->agent->is_mobile();
+		// $data['mobile'] = $ci->agent->mobile();
+		// $data['is_robot'] = $ci->agent->is_robot();
+		// $data['robot'] = $ci->agent->robot();
+		// $data['is_browser'] = $ci->agent->is_browser();
+		// $data['browser'] = $ci->agent->browser();
+		// $data['browser_ver'] = $ci->agent->version();
+		// $data['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+
+		// $newstr = date('Y-m-d H:i:s').' '. implode('|', $data) ."\r\n".$str;
+
+		// file_put_contents($file, $newstr);
+	} else if(isset($_REQUEST['width']) AND isset($_REQUEST['height'])) {
+    $_SESSION['screen_width'] = $_REQUEST['width'];
+    $_SESSION['screen_height'] = $_REQUEST['height'];
+    header('Location: ' . $_SERVER['PHP_SELF']);
+	} else {
+    echo '<script type="text/javascript">window.location = "' . $_SERVER['PHP_SELF'] . '?width="+screen.width+"&height="+screen.height;</script>';
+	}
 	
-	// $str = file_get_contents($file);
-	
-	// $newstr = date('Y-m-d H:i:s').' '.getenv('HTTP_FORWARDED').'|'.$_SERVER['REQUEST_METHOD'].'|'.$_SERVER['HTTP_USER_AGENT']."\r\n".$str;
-	
-	// file_put_contents($file, $newstr);
 };
--- app1/application/modules/z_libs/helpers/common_helper.php
+++ app1/application/modules/z_libs/helpers/common_helper.php
@@ -118,22 +118,40 @@ if ( ! function_exists('get_ip_address'))
     // else
 			// $ipaddress = 'UNKNOWN';
 		
-		if (getenv('HTTP_CLIENT_IP'))
-			$ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-			$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-			$ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-			$ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-			$ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-			$ipaddress = getenv('REMOTE_ADDR');
-    else
-			$ipaddress = 'UNKNOWN';
+		// if (getenv('HTTP_CLIENT_IP'))
+			// $ipaddress = getenv('HTTP_CLIENT_IP');
+    // else if(getenv('HTTP_X_FORWARDED_FOR'))
+			// $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    // else if(getenv('HTTP_X_FORWARDED'))
+			// $ipaddress = getenv('HTTP_X_FORWARDED');
+    // else if(getenv('HTTP_FORWARDED_FOR'))
+			// $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    // else if(getenv('HTTP_FORWARDED'))
+			// $ipaddress = getenv('HTTP_FORWARDED');
+    // else if(getenv('REMOTE_ADDR'))
+			// $ipaddress = getenv('REMOTE_ADDR');
+    // else
+			// $ipaddress = 'UNKNOWN';
+		
+    // return $ipaddress;
+		
+		if ( function_exists( 'apache_request_headers' ) ) {
+			$headers = apache_request_headers();
+		} else {
+			$headers = $_SERVER;
+		}
+		//Get the forwarded IP if it exists
+		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$the_ip = $headers['X-Forwarded-For'];
+		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )
+		) {
+			$the_ip = $headers['HTTP_X_FORWARDED_FOR'];
+		} else {
+			
+			$the_ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+		}
+		return $the_ip;
  
-    return $ipaddress;
 	}
 }
 
