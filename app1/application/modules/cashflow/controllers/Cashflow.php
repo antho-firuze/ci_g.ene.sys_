2c3479a3c422cd6b9cceb6f61a276ddb38fee5d3
antho-firuze <antho.firuze@gmail.com>
2017-10-11 11:34:33 +0700

    Fixbug: cf_inbound

--- app1/application/modules/cashflow/controllers/Cashflow.php
+++ app1/application/modules/cashflow/controllers/Cashflow.php
@@ -1329,6 +1329,12 @@ class Cashflow extends Getmeb
 			if (isset($this->params['for_inbound']) && !empty($this->params['for_inbound'])) {
 				if (isset($this->params['act']) && in_array($this->params['act'], ['new', 'cpy'])) {
 					$this->params['where_custom'] = "received_date is null";
+					$this->params['where_in']['t1.orgtrx_to_id'] = $this->_get_orgtrx();
+					if (! $result['data'] = $this->{$this->mdl}->{$this->c_method}($this->params)){
+						$this->xresponse(FALSE, ['data' => [], 'message' => $this->base_model->errors()]);
+					} else {
+						$this->xresponse(TRUE, $result);
+					}
 				}
 			}
 			
