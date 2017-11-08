270f273edf1c481bd38ee331aceca28cdc588e3c
antho-firuze <antho.firuze@gmail.com>
2017-11-08 16:57:14 +0700

    Update & adding for serialize "select element"

--- app1/assets/js/common.extend.func.js
+++ app1/assets/js/common.extend.func.js
@@ -7,30 +7,60 @@
 		type = type.toLowerCase();
 		
 		var o = {};
-		var a = this.serializeArray();
+		// Exclude Select Element
+		var a = this.find('[name]').not('select').serializeArray();
 		$.each(a, function (i, v) {
 			v.value = (v.value == 'on') ? '1' : v.value;
 			o[v.name] = o[v.name] ? o[v.name] || v.value : v.value;
+		});
+		// Only Select Element
+		var a = this.find('select').serializeArray();
+		$.each(a, function (i, v) {
+			if (o[v.name]) {
+				o[v.name] += ',' + v.value;
+			} else {
+				o[v.name] = v.value;
+			}
 		});
 		return (type == 'json') ? JSON.stringify(o) : o;
 	};
 
 	$.fn.serializeJSON = function () {
 		var o = {};
-		var a = this.serializeArray();
+		// Exclude Select Element
+		var a = this.find('[name]').not('select').serializeArray();
 		$.each(a, function (i, v) {
 			v.value = (v.value == 'on') ? '1' : v.value;
 			o[v.name] = o[v.name] ? o[v.name] || v.value : v.value;
+		});
+		// Only Select Element
+		var a = this.find('select').serializeArray();
+		$.each(a, function (i, v) {
+			if (o[v.name]) {
+				o[v.name] += ',' + v.value;
+			} else {
+				o[v.name] = v.value;
+			}
 		});
 		return JSON.stringify(o);
 	};
 
 	$.fn.serializeOBJ = function () {
 		var o = {};
-		var a = this.serializeArray();
+		// Exclude Select Element
+		var a = this.find('[name]').not('select').serializeArray();
 		$.each(a, function (i, v) {
 			v.value = (v.value == 'on') ? '1' : v.value;
 			o[v.name] = o[v.name] ? o[v.name] || v.value : v.value;
+		});
+		// Only Select Element
+		var a = this.find('select').serializeArray();
+		$.each(a, function (i, v) {
+			if (o[v.name]) {
+				o[v.name] += ',' + v.value;
+			} else {
+				o[v.name] = v.value;
+			}
 		});
 		return o;
 	};
